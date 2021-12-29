6f55011df600a4164903fee94baa12dbfdd06245
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2021-12-29 21:20:52 +0530

    finish menuconroller

--- controllers/menuContorller.js
+++ controllers/menuContorller.js
@@ -1,3 +1,36 @@
-const getAllItem = (req,res)=>{
-    res.json()
+
+const Product = require('../models/Product');
+
+const getAllItem = async (req,res)=>{
+    const result = await Product.find();
+    return res.json(result);
+}
+
+const addItem = async (req,res)=>{
+    if(!req.body.name || !req.body.price) return res.status(400).json({'message':'name and pice required'});
+    const newItem = {
+        name : req.body.name,
+        price : req.body.price
+    };
+    const result = await Product.create(newItem);
+    return res.status(201).json(result);
+}
+
+const updateItem = async (req,res)=>{
+    const id = !req.params?.id
+    if(!id) return res.sendStatus(203);
+    const currentItem = await Product.findById(id).exec();
+    if(req.body.name) currentItem.name = req.body.name;
+    if(req.body.price) currentItem.price = req.body.price;
+
+    const result =  await currentItem.save();
+    return res.status(200).json(result);
+    
+}
+
+const deleteItem = async (req,res)=>{
+    const id = !req.params?.id
+    if(!id) return res.sendStatus(203);
+    const result = await Product.findByIdAndDelete(id);
+    return res.status(200).json(result);
 }
